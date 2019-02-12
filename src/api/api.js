6ed4ce57bdf798446807851fd946890fc9ef1720
Noam Silver <noamsilver@gmail.com>
2019-02-12 13:07:43 +0200

    Added the api call methods

---
+++ src/api/api.js
@@ -0,0 +1,94 @@
+export default {
+  search: async (search) => {
+    const res = await fetch('http://int.v2x.foresightauto.com/stock-exchange-service/market/search', {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        searchString: search
+      }),
+    });
+    
+    return await getResObj(res);
+  },
+  buy: async (stock, quantity) => {
+    const res = await fetch('http://int.v2x.foresightauto.com/stock-exchange-service/market/buy', {
+      method: 'POST',
+      headers: {
+        Accept: '*/*',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        stockSymbol: stock,
+        stockQuantity: quantity,
+      }),
+    });
+    
+    return await getResObj(res);
+  },
+  sell: async (stock) => {
+    const res = await fetch('http://int.v2x.foresightauto.com/stock-exchange-service/market/sell', {
+      method: 'POST',
+      headers: {
+        Accept: '*/*',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        stockSymbol: stock,
+      }),
+    });
+    
+    return await getResObj(res);
+  },
+  portfolio: async () => {
+    const res = await fetch('http://int.v2x.foresightauto.com/stock-exchange-service/portfolio', {
+      mode: 'GET',
+      headers: {
+        Accept: '*/*'
+      }
+    })
+
+    return await getResObj(res);
+  },
+  market: async (stocks) => {
+    const query = stocks.reduce((queryStr, stock, index) => (queryStr += (index > 0 ?'&' : '') + `symbol=${stock}`), '?');
+    const res = await fetch(`http://int.v2x.foresightauto.com/stock-exchange-service/market${query}`, {
+      method: 'GET',
+      headers: {
+        Accept: '*/*',
+      },
+    });
+    
+    return await getResObj(res);
+  },
+  reset: async () => {
+    const res = await fetch('http://int.v2x.foresightauto.com/stock-exchange-service/management', {
+      mode: 'DELETE',
+      headers: {
+        Accept: '*/*'
+      }
+    })
+
+    return res.ok;
+  },
+};
+
+const getResObj = async (res) => {
+  if (res.status === 200) {
+    return await res.json();
+  } else if (res.status === 500) {
+    return {
+      err: await res.json(),
+    }
+  } else {
+    return {
+      err: {
+        status: res.status,
+        message: res.statusText,
+        error: res.statusText,
+      }
+    }
+  }
+}
